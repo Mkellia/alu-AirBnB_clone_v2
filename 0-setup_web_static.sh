d078ffa298e3258ab1edf63314dcee3b7e638c24
lscblack <l.christian@alustudent.com>
2024-06-05 13:01:29 +0200

    fixed bugs

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env bash
-# script that sets up web servers for the deployment of web_static
-sudo apt-get update
+# web_static development
+
+# Update and upgrade the system packages
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+# Install Nginx if it is not already installed
 sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+# Create directories for the web_static deployment
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+# Create a test HTML file
+echo "Hello, this is a test HTML file." | sudo tee /data/web_static/releases/test/0-index.html
 
+# Remove any existing symbolic link
+sudo rm -rf /data/web_static/current
+
+# Create a new symbolic link to the test release
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+# Change ownership of the /data/ directory to the ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+# Update the Nginx configuration to serve the content
+sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
+# Restart the Nginx service to apply the changes
 sudo service nginx restart
