f7228689c0f753217aadbc47f5b4905ab671fdba
lscblack <l.christian@alustudent.com>
2024-06-05 14:01:41 +0200

    config bug nailed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,46 +1,31 @@
 #!/usr/bin/env bash
-# web_static development
+#web_static development
 
-# Update and upgrade the system packages
 sudo apt-get -y update
 sudo apt-get -y upgrade
-
-# Install Nginx if it is not already installed
 sudo apt-get -y install nginx
-
-# Create directories for the web_static deployment
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-
-# Create the HTML file with the specified content
-cat <<EOF | sudo tee /data/web_static/releases/test/index.html
-<!DOCTYPE html>
-<html lang="en">
+echo "Hello, this is a test HTML file." | sudo tee /data/web_static/releases/test/index.html
+# Adding the new configuration for /hbnb_static/0-index.html
+echo "<!DOCTYPE html>
+<html lang=\"en\">
     <head>
-        <meta charset="UTF-8" />
+        <meta charset=\"UTF-8\" />
         <title>AirBnB clone</title>
     </head>
-    <body style="margin: 0px; padding: 0px;">
-        <header style="height: 70px; width: 100%; background-color: #FF0000">
+    <body style=\"margin: 0px; padding: 0px;\">
+        <header style=\"height: 70px; width: 100%; background-color: #FF0000\">
         </header>
 
-        <footer style="position: absolute; left: 0; bottom: 0; height: 60px; width: 100%; background-color: #00FF00; text-align: center; overflow: hidden;">
-            <p style="line-height: 60px; margin: 0px;">Holberton School</p>
+        <footer style=\"position: absolute; left: 0; bottom: 0; height: 60px; width: 100%; background-color: #00FF00; text-align: center; overflow: hidden;\">
+            <p style=\"line-height: 60px; margin: 0px;\">Holberton School</p>
         </footer>
     </body>
-</html>
-EOF
+</html>" | sudo tee /data/web_static/releases/test/0-index.html
+# End of new configuration
 
-# Remove any existing symbolic link
 sudo rm -rf /data/web_static/current
-
-# Create a new symbolic link to the test release
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Change ownership of the /data/ directory to the ubuntu user and group
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-
-# Update the Nginx configuration to serve the content
-sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}' /etc/nginx/sites-available/default
-
-# Restart the Nginx service to apply the changes
+sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
